b083987476efc9e752bafc7c31f067604c040112
Eric <twinpaireric@gmail.com>
2017-06-26 17:29:13 -0700

    Fix events button position on root page.

--- db/migrate/20170626235033_change_user_id_type_to_integer_using_explicit_cast.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserIdTypeToIntegerUsingExplicitCast < ActiveRecord::Migration[5.0]
-  def change
-  	change_column :comments, :user_id, 'integer USING CAST(user_id AS integer)'
-  end
-end
