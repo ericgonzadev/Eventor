428e35abcc71d62e2c97ca43a38ed4c3be79ad0d
Eric <twinpaireric@gmail.com>
2017-09-12 23:13:00 -0700

    Enable leverage browser caching for static resources

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Enable leverage browser caching for static resources
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
