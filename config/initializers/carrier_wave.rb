8203b74f0b8777c1dc1d277841ecfa4a1d18fee1
Eric Gonzalez <ericgonzadev@gmail.com>
2026-04-20 16:04:01 -0700

    Use postgres in prod

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region:                ENV['S3_REGION']
+      region:                ENV.fetch('S3_REGION', 'us-west-1')
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
